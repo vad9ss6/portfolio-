5f2721ecd262f1ab6caf3150f3fa81a3369439e8
vad9ss6 <vad9ss6@gmail.com>
2021-01-06 22:48:46 +0300

    added toggle menu button

--- src/Component/Header/HeaderNav/HeaderNav.tsx
+++ src/Component/Header/HeaderNav/HeaderNav.tsx
@@ -1,19 +1,27 @@
 import React, {FC} from 'react';
 import s from './HeaderNav.module.css';
 import linkStyle from './../../../common/styles/Link.module.css'
+import { Link } from 'react-scroll'
+
 type HeaderNavPropsType = {
     isActive: boolean
+    toggleButton: () => void
 }
 
-export const HeaderNav:FC<HeaderNavPropsType> = ({isActive}) => {
+
+export const HeaderNav:FC<HeaderNavPropsType> = ({isActive,toggleButton}) => {
     const styleHeaderNavAnimate = {
         left: isActive? '0' : ''
     }
+
+    const closeMenu = () =>{
+        toggleButton()
+    }
     return <div style={styleHeaderNavAnimate} className={s.headerNav}>
-        <a href="#" className={linkStyle.link}>Главная</a>
-        <a href="#" className={linkStyle.link}>Мои скилы</a>
-        <a href="#" className={linkStyle.link}>Мои работы</a>
-        <a href="#" className={linkStyle.link}>Контакты</a>
+        <Link activeClass={s.active} to={'Header'} spy={true} smooth={true} duration={1000} className={linkStyle.link} onClick={closeMenu}>Главная</Link>
+        <Link activeClass={s.active} to={'MySkills'} spy={true} smooth={true} duration={1000} className={linkStyle.link} onClick={closeMenu}>Мои скилы</Link>
+        <Link activeClass={s.active} to={'MySkills'} spy={true} smooth={true} duration={1000} className={linkStyle.link} onClick={closeMenu}>Мои работы</Link>
+        <Link activeClass={s.active} to={'MySkills'} spy={true} smooth={true} duration={1000} className={linkStyle.link} onClick={closeMenu}>Контакты</Link>
     </div>
 }
 
