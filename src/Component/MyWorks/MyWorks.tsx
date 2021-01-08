e959061130bcb4c22c8aff5905057f6cba70819c
vad9ss6 <vad9ss6@gmail.com>
2021-01-08 04:29:48 +0300

    added component works

--- src/Component/MyWorks/MyWorks.tsx
+++ src/Component/MyWorks/MyWorks.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styleContainer from "../../common/styles/Container.module.css";
-import bgOpacity from "./../../common/styles/bgOpacity.module.css"
 import {Works} from "./Works/Works";
 import bg from "../../common/img/headerBg/3.jpg";
+import {myWorksType} from "../../App";
 
+type PropType = {
+    works:Array<myWorksType>
+}
 
-export const MyWorks = () => {
+export const MyWorks = (props: PropType) => {
     return <div className={styleContainer.main_block} style={{background: `center / cover no-repeat url(${bg})`, backgroundAttachment: 'fixed'}} id='MyWorks'>
 
-        <h2 className={styleContainer.main_title}>Мои Работы</h2>
+        <h2 className={styleContainer.main_title}>My works</h2>
         <div className={ styleContainer.container }>
-           <Works />
+            {props.works.map(w => <Works key={w.id} id={w.id} imgWork={w.imgWork} title={w.title} description={w.description}/>)}
         </div>
     </div>
 
--- src/Component/MyWorks/Works/Works.tsx
+++ src/Component/MyWorks/Works/Works.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import s from './Works.module.css'
+import {myWorksType} from "../../../App";
 
-export const Works = () => {
+type PropType = myWorksType
+
+export const Works: React.FC<PropType> = ({id, imgWork, title, description}) => {
     return <div className={s.card}>
-        <div className={s.img}>img</div>
-        <div className={s.title}>title</div>
-        <div className={s.description}>desc</div>
+        <div className={s.img}>
+            <img src={imgWork} alt="work" className={s.workImg}/>
+        </div>
+        <div className={s.title}>{title}</div>
+        <div className={s.description}>{description}</div>
     </div>
 }
